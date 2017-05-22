3c2bd57e9e01082f6c2152e4760efe6e7f53b38a
imnakez <imnakez@yandex.ru>
2017-05-22 17:58:20 +0300

    fixed error on category page before update

--- com_html5flippingbook/site/views/html5flippingbook/tmpl/default.php
+++ com_html5flippingbook/site/views/html5flippingbook/tmpl/default.php
@@ -14,6 +14,9 @@ JHTML::_('behavior.modal', 'a.html5-modal');
 JHtml::_('bootstrap.framework');
 JHtml::_('bootstrap.loadCss');
 $colNumb = JFactory::getApplication()->getMenu()->getActive()->params->get('c_colnumb');
+if (!$colNumb) {
+	$colNumb = 1;
+}
 $uri = JUri::getInstance();
 $jinput = JFactory::$application->input;
 
